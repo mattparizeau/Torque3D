9973ca65a48a1fdb7d3c501099360204a0063668
Jeff Hutchinson <programmerjeff95@gmail.com>
2015-01-28 14:12:16 -0500

    scatter sky fix - remove unused zposition

--- Templates/Full/game/shaders/common/gl/scatterSkyP.glsl
+++ Templates/Full/game/shaders/common/gl/scatterSkyP.glsl
@@ -31,12 +31,10 @@ in vec4  mieColor;
 #define IN_mieColor mieColor
 in vec3  v3Direction;
 #define IN_v3Direction v3Direction
-in float zPosition;
-#define IN_zPosition zPosition
 in vec3  pos;
 #define IN_pos pos
 
-uniform samplerCube nightSky ;
+uniform samplerCube nightSky;
 uniform vec4 nightColor;
 uniform vec2 nightInterpAndExposure;
 uniform float useCubemap;
@@ -46,31 +44,25 @@ uniform vec3 sunDir;
 out vec4 OUT_col;
 
 void main()
-{ 
+{
 
    float fCos = dot( lightDir, IN_v3Direction ) / length(IN_v3Direction);
    float fCos2 = fCos*fCos;
-    
+
    float g = -0.991;
    float g2 = -0.991 * -0.991;
 
    float fMiePhase = 1.5 * ((1.0 - g2) / (2.0 + g2)) * (1.0 + fCos2) / pow(abs(1.0 + g2 - 2.0*g*fCos), 1.5);
-   
+
    vec4 color = IN_rayleighColor + fMiePhase * IN_mieColor;
    color.a = color.b;
-   
+
    vec4 nightSkyColor = texture(nightSky, -v3Direction);
    nightSkyColor = mix(nightColor, nightSkyColor, useCubemap);
 
    float fac = dot( normalize( pos ), sunDir );
    fac = max( nightInterpAndExposure.y, pow( clamp( fac, 0.0, 1.0 ), 2 ) );
    OUT_col = mix( color, nightSkyColor, nightInterpAndExposure.y );
-   
-   // Clip based on the camera-relative
-   // z position of the vertex, passed through
-   // from the vertex position.
-   if(zPosition < 0.0)
-      discard;
 
    OUT_col.a = 1;
    OUT_col = hdrEncode( OUT_col );
